d711a57b79af455d5fb7a32f92344cd6f5fb0df0
caytis <ashleyolson123@hotmail.com>
2023-09-07 22:59:28 -0600

    nearly done

--- comprehensions/comp.py
+++ comprehensions/comp.py
@@ -1,49 +1,48 @@
+from collections import namedtuple
 
-partsFILE = 'comprehensions\parts.txt'
+
+partsFILE = 'comprehensions\parts.txt' # comprehensions\
 projectsFILE = 'comprehensions\projects.txt'
 suppliersFILE = 'comprehensions\suppliers.txt'
 spjFILE = 'comprehensions\spj.txt'
 
+parts = list()
+projects = set()
+suppliers = set()
+spj = set()
+
 
 def makeTuples():
+    global parts
+    global projects
+    global suppliers
+    global spj
     parts = readFile(partsFILE)
     projects = readFile(projectsFILE)
     suppliers = readFile(suppliersFILE)
     spj = readFile(spjFILE)
-    
-    return parts, projects, suppliers, spj
 
 def readFile(FILE):
-    data = list()
-    names = list()
+    all = list()
 
     with open(FILE) as file:
 
         i = 0
         for line in file:
-            values = dict()
-            this = line.split(',')
+            this = line.strip().split(',')
 
             if i == 0:
-                values['name'] = this[0].strip() #??? TODO: need to make this accessible
+                name = this[0].strip()
+                i += 1
             elif i == 1:
-                for word in this:
-                    # if i == 1:
-                    #     beginning = word[0]
-                    names.append(word.strip())
+                data = namedtuple(name, this)
+                i += 1
             else:
-                x = 0
-                for word in this:
-                    values[names[x] + str(i - 1)] = word.strip()
-                    x += 1
-                data.append(values)
-            i += 1
+                all.append(data._make(this))
+                
     file.close()
-    return data
+    return all
 
-
-def get(lyst):
-    pass
 
 # {s.sname for s in suppliers for r in spj for j in projects 
 #  if r.sno == s.sno if j.jno == r.jno if j.city == 'London'}
@@ -64,14 +63,20 @@ def get(lyst):
 # # {'Paris': {'Jones', 'Blake'}, 'Athens': {'Adams'}, 'London': {'Clark', 'Smith'}}
 
 def main():
-    parts, projects, suppliers, spj = makeTuples()
-    get(suppliers, "bolts")
-    # # 1. Get names of all suppliers that supply bolts.
-    # # 2. Get names of all suppliers that supply blue parts.
-    # # 3. Get names of all suppliers not used in Athens projects
-    # # 4. Get names and colors of all parts not used in Oslo
-    # # 5. Get pairs of names of all suppliers that are located in the same city.
-    # # 6. Print all suppliers out by city
+    makeTuples()
+
+    print({s.sname for s in suppliers for r in spj for p in parts if r.sno == s.sno if p.pno == r.pno if p.pname == "Bolt"}) #1. Get names of all suppliers that supply bolts.
+
+    print({s.sname for s in suppliers for r in spj for p in parts if r.sno == s.sno if p.pno == r.pno if p.color == "Blue"}) #2. Get names of all suppliers that supply blue parts.
+
+    print({s.sname for s in suppliers for r in spj for j in projects if r.sno == s.sno if j.jno == r.jno if j.city != "Athens"}) #3. Get names of all suppliers not used in Athens projects
+
+    #4. Get names and colors of all parts not used in Oslo
+
+    #5. Get pairs of names of all suppliers that are located in the same city.
+
+    #6. Print all suppliers out by city
+
 
 if __name__ == '__main__':
     main()
