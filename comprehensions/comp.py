a84d966151d9f1e81dac6f4d1345e93562f2204b
AshOlson <10987957@uvu.edu>
2023-09-08 22:49:30 -0600

    4 and 5

--- comprehensions/comp.py
+++ comprehensions/comp.py
@@ -75,9 +75,13 @@ def main():
     ath_not = {s.sname for s in suppliers if s.sname not in ath_supps}
     print(ath_not)
 
-    #4. Get names and colors of all parts not used in Oslo (not A, B), (A, not B, C)
+    os_projs = {j.jno for j in projects if j.city == 'Oslo'} #4. Get names and colors of all parts not used in Oslo
+    os_part_ids = {l.pno for l in spj for jno in os_projs if l.jno == jno}
+    os_parts = {(p.pname, p.color) for p in parts for pno in os_part_ids if p.pno != pno}
+    print(os_parts)
 
-    #5. Get pairs of names of all suppliers that are located in the same city.
+    name_pairs = {(n1.sname, n2.sname) for n1 in suppliers for n2 in suppliers if n1.sno != n2.sno if n1.city == n2.city} #5. Get pairs of names of all suppliers that are located in the same city.
+    # print({p for p in name_pairs if p[0] != p[1]})
 
     #6. Print all suppliers out by city
 
