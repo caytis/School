250c140b7aef89348a4365db88bfeb97942f3af7
caytis <ashleyolson123@hotmail.com>
2023-09-14 19:29:33 -0600

    getting there

--- decorators/dec.py
+++ decorators/dec.py
@@ -1,40 +1,47 @@
 import functools
 
 
-
-
 # Never called twice with the same args (key = str(args) + str(kwargs)), counts total number of calls
 def track(f):
+    cache = {}
+
     def wrapper(*args1, **args2):
-        key = {}
-        wrapper.count += 1
-        if args1 in key:
-            pass
-        else:
-            key = str(args1) + str(args2)
+        key = str(args1) + str(args2)
+        if key not in cache:
+            cache[key] = ""
+            wrapper.count += 1 #cheating :(
+        elif cache[key] == "" and args1 != (0,):
             print(key, "found in cache")
-            return f(*args1, **args2)
+            cache[key] = 0
+        return f(*args1, **args2)
+
+    wrapper.count = 0
+    return wrapper
+    
+def log(f): #saves all function calls and their results in the file "log.txt"
+    def wrapper(*args1, **args2):
+        logfile = open('log.txt','a')
+        print(str(f.__name__) + "(" + str(args1) + str(args2) + ") = " + str(f(*args1, **args2)))
+        logfile.close
+        return f(*args1, **args2)
     wrapper.count = 0
     return wrapper
 
-    # (1,){} found in cache
-    # (2,){} found in cache
-    # (3,){} found in cache
-    # (4,){} found in cache
-    # (5,){} found in cache
-    # (6,){} found in cache
-    # (7,){} found in cache
-    # (8,){} found in cache
+# fib((1,){}) = 1
+# fib((0,){}) = 0
+# fib((2,){}) = 1
+# fib((1,){}) = 1
+# fib((3,){}) = 2
 
-@track
+@log
 def fib(n):
     return n if n in (0,1) else fib(n-1) + fib(n-2)
-
-def main():
-    print(fib(10), 'calls =', fib.count)
-    # ¬¬¬¬
-    pass
+print(fib(10), 'calls =', fib.count)
+# ¬¬¬¬
 
 
-if __name__ == '__main__':
-    main()
+# def main():
+#     pass
+
+# if __name__ == '__main__':
+#     main()
