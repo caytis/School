04146f1f72e5143828de489f2f3e44a7cfa238cc
caytis <ashleyolson123@hotmail.com>
2023-09-14 20:32:33 -0600

    trying more stuff

--- decorators/dec.py
+++ decorators/dec.py
@@ -6,25 +6,23 @@ def track(f):
     cache = {}
 
     def wrapper(*args1, **args2):
-        key = str(args1) + str(args2)
-        if key not in cache:
-            cache[key] = ""
-            wrapper.count += 1 #cheating :(
-        elif cache[key] == "" and args1 != (0,):
-            print(key, "found in cache")
-            cache[key] = 0
+        # key = str(args1) + str(args2)
+        # if key not in cache:
+        #     cache[key] = ""
+        #     wrapper.count += 1 #cheating :(
+        # elif cache[key] == "" and args1 != (0,):
+        #     print(key, "found in cache")
+        #     cache[key] = 0
         return f(*args1, **args2)
-
     wrapper.count = 0
     return wrapper
     
 def log(f): #saves all function calls and their results in the file "log.txt"
     def wrapper(*args1, **args2):
-        logfile = open('log.txt','a')
-        print(str(f.__name__) + "(" + str(args1) + str(args2) + ") = " + str(f(*args1, **args2)))
+        logfile = open('decorators\log.txt','a')
+        logfile.write(f.__name__ + "(" + str(args1) + str(args2) + ") = " + "\n")
         logfile.close
         return f(*args1, **args2)
-    wrapper.count = 0
     return wrapper
 
 # fib((1,){}) = 1
@@ -33,11 +31,12 @@ def log(f): #saves all function calls and their results in the file "log.txt"
 # fib((1,){}) = 1
 # fib((3,){}) = 2
 
-@log
+@track
+# @log
 def fib(n):
     return n if n in (0,1) else fib(n-1) + fib(n-2)
-print(fib(10), 'calls =', fib.count)
-# ¬¬¬¬
+print(fib(10))
+# , 'calls =', fib.count¬¬¬¬
 
 
 # def main():
