c3997a42c65d543e8c2fb821d538def74cdd48cb
caytis <ashleyolson123@hotmail.com>
2023-09-14 23:34:20 -0600

    DONE!!!

--- decorators/dec.py
+++ decorators/dec.py
@@ -1,47 +1,29 @@
-import functools
 
-
-# Never called twice with the same args (key = str(args) + str(kwargs)), counts total number of calls
 def track(f):
     cache = {}
-    @functools.wraps(f)
     def wrapper(*args1, **args2):
         key = str(args1) + str(args2)
         if key not in cache:
-            cache[key] = ""
-            wrapper.count += 1 #cheating :(
-        elif cache[key] == "" and args1 != (0,):
+            wrapper.count += 1
+            result = f(*args1, **args2)
+            cache[key] = result
+        else:
             print(key, "found in cache")
-            cache[key] = 0
-            # return f.__wrapped__(*args1, **args2)
-        return f(*args1, **args2)
+        return cache[key]
     wrapper.count = 0
     return wrapper
     
-def log(f): #saves all function calls and their results in the file "log.txt"
+def log(f):
     def wrapper(*args1, **args2):
-        logfile = open('decorators\log.txt','a')
-        logfile.write(f.__name__ + "(" + str(args1) + str(args2) + ") = " + "\n")
-        logfile.close
-        return f(*args1, **args2)
+        result = f(*args1, **args2)
+        with open('decorators\log.txt','a') as logfile:
+            logfile.write(f.__name__ + "(" + str(args1) + str(args2) + ") = " + str(result) + "\n")
+            logfile.close
+        return result
     return wrapper
 
-# fib((1,){}) = 1
-# fib((0,){}) = 0
-# fib((2,){}) = 1
-# fib((1,){}) = 1
-# fib((3,){}) = 2
-
 @track
-# @log
+@log
 def fib(n):
-    return n if n in (0,1) else fib(n-1) + fib(n-2)
-print(fib(10))
-# , 'calls =', fib.count¬¬¬¬
-
-
-# def main():
-#     pass
-
-# if __name__ == '__main__':
-#     main()
+   return n if n in (0,1) else fib(n-1) + fib(n-2)
+print(fib(10), 'calls =', fib.count) # No comma unless I change this to str() :(
