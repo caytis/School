8e3bf6e0a12a88e0664fb2fedfd7071c6da77032
AshOlson <123228862+caytis@users.noreply.github.com>
2023-10-03 00:42:17 -0600

    picked

--- wordgame/wordgame.py
+++ wordgame/wordgame.py
@@ -1,29 +1,33 @@
 import itertools.groupby, random.choice, random.shuffle
 
 def start():
-    inp = ""
+    # inp = ""
     with open("words.txt") as file:
         words = file.readlines()
         for i in words:     # Start at 0? i - 1?
             words[i] = words[i].strip()
-        while inp != "q":
+        while True:
             inp = input("Enter the range of word lengths (low,high):")
             inp.strip()
+            if inp == "q":
+                break
             rang = inp.split(",")
             low = rang[0]
             high = rang[1]
             word = pick(words, high)
             found = search(word, words, low, high)
             write(found)
+            # continue
 
 def pick(words, length):
     words = random.shuffle(words)
     for word in words:
         if len(word) == length:
-            pass
+            return word
+    print("No words in this range found")
 
 def search(it, words, low, high):
-    pass
+    long = itertools.groupby(words, lambda x : len(x) >= low and len(x) <= high) # lambda?
 
 def write(found):
     pass
