710d7d158dc8ab5af5d431c4b0a0874b1164d3ed
AshOlson <ashleyolson123@hotmail.com>
2023-11-03 00:47:16 -0600

    lyst_dir

--- HTML/HTMLindex.py
+++ HTML/HTMLindex.py
@@ -1,4 +1,12 @@
+import os
 
+def search_dir(dir):
+    """Search curr dir, make list with subdirs and images"""
+    lyst = os.scandir(dir)
+    names = list(entry.name for entry in lyst if entry.is_dir() or entry.is_file())
+    for name in names:
+        print(name)
 
 if __name__ == '__main__':
+    search_dir("HTML/Pictures")
     pass
